ae77859129dfed798564b25504570bfe76fe86f4
rishabhguptajs <rishabhgupta4523@gmail.com>
2023-12-04 21:32:24 +0530

    added website pre-loader

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const copyright = document.getElementById('date-copyright');
 const date = new Date();
 const year = date.getFullYear();
 copyright.textContent = year;
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.querySelector(".preloader").style.display = "none";
+    }, 2000);
+});
